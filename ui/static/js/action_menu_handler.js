047b1c974bdd5a245d91157ad4334d53340595b1
jebbs <qjebbs@gmail.com>
2019-07-23 12:44:37 +0800

    fix item meta links

--- ui/static/js/action_menu_handler.js
+++ ui/static/js/action_menu_handler.js
@@ -1,65 +1,66 @@
-class ActionMenuHandler {
-    constructor(element) {
-        this.element = element;
-        document.querySelectorAll(".current-item")
-            .forEach(e => e.classList.remove("current-item"));
-        this.element.classList.add("current-item");
-    }
-    show() {
-        ModalHandler.close();
-        let template = document.getElementById("action-menus");
-        if (template === null) return;
+function handleActionMenu(element) {
+    let currentEntry = findEntry(element);
+    if (!currentEntry) return;
+    // if (isListView()) highlightEntry(currentEntry);
 
-        if (this.element.classList.contains("item")) {
-            // menu for entries
-            initMenu(this.element.querySelectorAll(".item-meta a"), "entries");
-            document.querySelector("#menu-mark-above-read").addEventListener("click", () => {
-                setEntriesAboveStatusRead(this.element);
-                ModalHandler.close();
-            });
-        } else if (this.element.classList.contains("entry")) {
-            // menu for entry
-            initMenu(this.element.querySelectorAll(".entry-actions a"), "entry");
-        }
-        // cancel menu
-        document.querySelector("#menu-action-cancel").addEventListener("click", () => {
+    ModalHandler.close();
+    let template = document.getElementById("action-menus");
+    if (template === null) return;
+
+    if (currentEntry.classList.contains("item")) {
+        // menu for entries
+        initMenu(currentEntry.querySelectorAll(".item-meta a"), "entries");
+        document.querySelector("#menu-mark-above-read").addEventListener("click", () => {
+            setEntriesAboveStatusRead(currentEntry);
             ModalHandler.close();
         });
+    } else if (currentEntry.classList.contains("entry")) {
+        // menu for entry
+        initMenu(currentEntry.querySelectorAll(".entry-actions a"), "entry");
+    }
+    // cancel menu
+    document.querySelector("#menu-action-cancel").addEventListener("click", () => {
+        ModalHandler.close();
+    });
 
-        // initMenu creates menu for given links in action modal.
-        // dataForValue specifies the part of predefined menu to keep, 
-        // which have the given value for "data-for" attribute.
-        function initMenu(links, dataForValue) {
-            ModalHandler.open(template.content, true);
-            let list = document.querySelector(".action-menus #element-links");
-            while (list.hasChildNodes()) {
-                list.removeChild(list.firstChild);
-            }
+    function highlightEntry(entry) {
+        document.querySelectorAll(".current-item")
+            .forEach(e => e.classList.remove("current-item"));
+        entry.classList.add("current-item");
+    }
+    // initMenu creates menu for given links in action modal.
+    // dataForValue specifies the part of predefined menu to keep, 
+    // which have the given value for "data-for" attribute.
+    function initMenu(links, dataForValue) {
+        ModalHandler.open(template.content, true);
+        let list = document.querySelector(".action-menus #element-links");
+        while (list.hasChildNodes()) {
+            list.removeChild(list.firstChild);
+        }
 
-            links.forEach(
-                link => {
-                    if (link.dataset.actionMenuExcluded !== undefined) return;
-                    let menu = document.createElement("li");
-                    menu.innerText = link.innerText;
-                    menu.addEventListener("click", () => {
-                        clickElement(link);
-                        ModalHandler.close();
-                    });
-                    list.appendChild(menu);
-                }
-            );
-
-            document.querySelectorAll(".action-menus li[data-for]")
-                .forEach(e => {
-                    if (e.dataset.for !== dataForValue)
-                        e.style.display = 'none';
+        links.forEach(
+            link => {
+                if (link.dataset.actionMenuExcluded !== undefined) return;
+                let menu = document.createElement("li");
+                menu.innerText = link.innerText;
+                menu.addEventListener("click", () => {
+                    clickElement(link);
+                    ModalHandler.close();
                 });
-        }
+                list.appendChild(menu);
+            }
+        );
 
-        function clickElement(element) {
-            let e = document.createEvent("MouseEvents");
-            e.initEvent("click", true, true);
-            element.dispatchEvent(e);
-        }
+        document.querySelectorAll(".action-menus li[data-for]")
+            .forEach(e => {
+                if (e.dataset.for !== dataForValue)
+                    e.style.display = 'none';
+            });
+    }
+
+    function clickElement(element) {
+        let e = document.createEvent("MouseEvents");
+        e.initEvent("click", true, true);
+        element.dispatchEvent(e);
     }
 }
